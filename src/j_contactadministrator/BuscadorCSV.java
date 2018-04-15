b2035192a04c9ea47e755e8ecae79776a5ec05f0
galloafranco <galloafranco@gmail.com>
2018-04-15 15:26:55 -0300

    Emprolijamos codigo en BuscadorCSV, y modificamos el archivos Contactos.csv ya que nos estaba generando errores en nuestro proceso de importacion, no se ajustaba a las especificaciones demandadas por el software.

--- src/j_contactadministrator/BuscadorCSV.java
+++ src/j_contactadministrator/BuscadorCSV.java
@@ -21,31 +21,7 @@ public class BuscadorCSV extends Thread{
     public void run() {
     
     while(continuador){
-    
-        //----------------------------------------------------------------------
-        //---------------------DETECTOR DE ARCHIVOS-----------------------------
-//        try{
-//            System.out.println("Se estan buscando archivos de extension .csv ...");
-//            
-//        File ruta = new File("D:\\Franco\\JAVA\\Proyectos\\J_ContactAdministrator");
-//        String[] archivos = ruta.list();
-//        
-//        int contador = 0;//variable auxiliar   
-//        for(String directorio : archivos){
-////            BuscadorCSV.sleep(500);
-//            if(directorio.contains(".csv")){
-//                contador++;
-////                System.out.println(directorio);
-//            }
-//        }
-//        }catch(InterruptedException e){
-//            e.getMessage();
-//        }
-        
-//        System.out.println("Cantidad de archivos .csv encontrados :" + contador);
-//        System.out.println("-------------------------------------------------");
-       
-        //----------------------------------------------------------------------
+
         //---------------------------IMPORTACION--------------------------------
         
         //Se crea una instancia de la clase encargada de la persistencia de datos
@@ -55,65 +31,67 @@ public class BuscadorCSV extends Thread{
         BufferedReader br = null;
 
         try{
-          File ruta2 = new File("D:\\Franco\\JAVA\\Proyectos\\J_ContactAdministrator\\Contactos.csv");  
-          File rutaOriginalFichero;
-            try (FileReader fr = new FileReader(ruta2)) {
-                br = new BufferedReader(fr);
-                boolean salidor = true;
-//          System.out.println("Nos conectaremos a la base de datos para insertar la nueva informacion..");
-                while (salidor) {
-                    //Lectura de la linea entera
-                    String line = br.readLine();
-                    
-                    if(line != null){
-                        //division de la linea por coma mediante el metodo split
-                        String[] campos = line.split(";");
-                        
+            File ruta2 = new File("D:\\Franco\\JAVA\\Proyectos\\J_ContactAdministrator\\Contactos.csv");  
+            FileReader fr = new FileReader(ruta2);
+            br = new BufferedReader(fr);
+          
+            boolean salidor = true;
+
+                while(salidor){
+                
+                //Lectura de la linea entera
+                String line = br.readLine();
+              
+                if(line != null){
+                
+                //division de la linea por coma mediante el metodo split
+                String[] campos = line.split(";");
+              
 //              System.out.println("Datos encontrados en el archivo: ");
 //              System.out.println(Arrays.toString(campos));
-
-String nombre = campos[0];
-String apellido = campos[1];
-String alias = campos[2];
-String mail = campos[3];
-String direccion = campos[4];
-String numero = campos[5];
-
-//Se instancia un Contacto con las variables reunidas del array campos
-Contacto contactoImportado = new Contacto(nombre, apellido, alias, mail, direccion, numero);
-//Se agrega el contacto al sistema de persistencia del programa
-//                  System.out.println("Nos conectaremos a la base de datos para insertar la nueva informacion..");
-agendaParaImportaciones.addContacto(contactoImportado);
-
-                    } else {
-//                  System.out.println("Se instertaron los datos a la DB ");
-salidor = false;
-                    }
+              
+                String nombre = campos[0];
+                String apellido = campos[1];
+                String alias = campos[2];
+                String mail = campos[3];
+                String direccion = campos[4];
+                String numero = campos[5];
+              
+                //Se instancia un Contacto con las variables reunidas del array campos 
+                Contacto contactoImportado = new Contacto(nombre, apellido, alias, mail, direccion, numero);
+                //Se agrega el contacto al sistema de persistencia del programa
+                agendaParaImportaciones.addContacto(contactoImportado);
+            
+                } else {
+                salidor = false;
                 }
-                //------------------------------------------------------------------
-                //Nos preparamos para realizar la copia del archivo ya almacenado
-                File rutaDestinoFichero;
-                rutaOriginalFichero= new File("D:\\Franco\\JAVA\\Proyectos\\J_ContactAdministrator\\Contactos.csv");
-                rutaDestinoFichero = new File("D:\\Franco\\JAVA\\Proyectos\\J_ContactAdministrator\\ContactosLeidos\\Contactos.csv");
-                InputStream in = new FileInputStream(rutaOriginalFichero);
-                OutputStream out = new FileOutputStream(rutaDestinoFichero);
-                //Usamos un buffer para la copia
-                byte[] buf = new byte[1024];
-                int len;
-                while ((len = in.read(buf)) > 0) {
-                    out.write(buf, 0, len);
-                } in.close();
-                out.close();
-                //cerramos el FileReader para poder borrar el archivo
+                }
+           
+        //-------------------------COPIA DEL ARCHIVO----------------------------
+        
+            File rutaOriginalFichero;
+            File rutaDestinoFichero;
+            rutaOriginalFichero= new File("D:\\Franco\\JAVA\\Proyectos\\J_ContactAdministrator\\Contactos.csv");
+            rutaDestinoFichero = new File("D:\\Franco\\JAVA\\Proyectos\\J_ContactAdministrator\\ContactosLeidos\\Contactos.csv");
+              
+            InputStream in = new FileInputStream(rutaOriginalFichero);
+            OutputStream out = new FileOutputStream(rutaDestinoFichero);
+              
+            //Usamos un buffer para la copia
+            byte[] buf = new byte[1024];
+            int len;
+            while ((len = in.read(buf)) > 0) {
+                out.write(buf, 0, len);
             }
-            //------------------------------------------------------------------    
-              //Aqui generamos el delete para no volver a importar el mismo archivo
-              boolean borrarOriginal = rutaOriginalFichero.delete();
-              if(borrarOriginal){
-//                      System.out.println("Se borro el archivo original de manera satisfactoria");
-//                      System.out.println("");
-              }else
-                      System.err.println("-----*No se pudo borrar el archivo*-----");
+            in.close();
+            out.close();
+              
+            fr.close();//cerramos el FileReader para poder borrar el archivo
+            
+        //----------------------BORRAR ARCHIVO ORIGINAL-------------------------    
+              
+            //Aqui generamos el delete para no volver a importar el mismo archivo
+            boolean borrarOriginal = rutaOriginalFichero.delete();
         
         }catch(FileNotFoundException e){
             e.getMessage();
@@ -140,6 +118,8 @@ salidor = false;
     /*
     ****************************************************************************
     */
+    
+    //METODO PARA DETENER EL THREAD
     public void detener(){
     continuador = false;
     }
