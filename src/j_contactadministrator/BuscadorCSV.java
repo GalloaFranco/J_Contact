4204b9521e7e932ecbfebe1b1352e2b5fd86068c
galloafranco <galloafranco@gmail.com>
2018-04-15 13:47:46 -0300

    comentamos en BuscadorCSV partes de codigo que no tenian utilidad

--- src/j_contactadministrator/BuscadorCSV.java
+++ src/j_contactadministrator/BuscadorCSV.java
@@ -26,18 +26,18 @@ public class BuscadorCSV extends Thread{
         //---------------------DETECTOR DE ARCHIVOS-----------------------------
 //        try{
 //            System.out.println("Se estan buscando archivos de extension .csv ...");
-            
-        File ruta = new File("D:\\Franco\\JAVA\\Proyectos\\J_ContactAdministrator");
-        String[] archivos = ruta.list();
-        
-        int contador = 0;//variable auxiliar   
-        for(String directorio : archivos){
-//            BuscadorCSV.sleep(500);
-            if(directorio.contains(".csv")){
-                contador++;
-//                System.out.println(directorio);
-            }
-        }
+//            
+//        File ruta = new File("D:\\Franco\\JAVA\\Proyectos\\J_ContactAdministrator");
+//        String[] archivos = ruta.list();
+//        
+//        int contador = 0;//variable auxiliar   
+//        for(String directorio : archivos){
+////            BuscadorCSV.sleep(500);
+//            if(directorio.contains(".csv")){
+//                contador++;
+////                System.out.println(directorio);
+//            }
+//        }
 //        }catch(InterruptedException e){
 //            e.getMessage();
 //        }
@@ -56,59 +56,56 @@ public class BuscadorCSV extends Thread{
 
         try{
           File ruta2 = new File("D:\\Franco\\JAVA\\Proyectos\\J_ContactAdministrator\\Contactos.csv");  
-          FileReader fr = new FileReader(ruta2);
-          br = new BufferedReader(fr);
-          
-          boolean salidor = true;
+          File rutaOriginalFichero;
+            try (FileReader fr = new FileReader(ruta2)) {
+                br = new BufferedReader(fr);
+                boolean salidor = true;
 //          System.out.println("Nos conectaremos a la base de datos para insertar la nueva informacion..");
-          while(salidor){
-              //Lectura de la linea entera
-              String line = br.readLine();
-              
-              if(line != null){
-              //division de la linea por coma mediante el metodo split
-              String[] campos = line.split(";");
-              
+                while (salidor) {
+                    //Lectura de la linea entera
+                    String line = br.readLine();
+                    
+                    if(line != null){
+                        //division de la linea por coma mediante el metodo split
+                        String[] campos = line.split(";");
+                        
 //              System.out.println("Datos encontrados en el archivo: ");
 //              System.out.println(Arrays.toString(campos));
-              
-              String nombre = campos[0];
-              String apellido = campos[1];
-              String alias = campos[2];
-              String mail = campos[3];
-              String direccion = campos[4];
-              String numero = campos[5];
-              
-              //Se instancia un Contacto con las variables reunidas del array campos 
-              Contacto contactoImportado = new Contacto(nombre, apellido, alias, mail, direccion, numero);
-              //Se agrega el contacto al sistema de persistencia del programa
+
+String nombre = campos[0];
+String apellido = campos[1];
+String alias = campos[2];
+String mail = campos[3];
+String direccion = campos[4];
+String numero = campos[5];
+
+//Se instancia un Contacto con las variables reunidas del array campos
+Contacto contactoImportado = new Contacto(nombre, apellido, alias, mail, direccion, numero);
+//Se agrega el contacto al sistema de persistencia del programa
 //                  System.out.println("Nos conectaremos a la base de datos para insertar la nueva informacion..");
-              agendaParaImportaciones.addContacto(contactoImportado);
-            
-                } else {
+agendaParaImportaciones.addContacto(contactoImportado);
+
+                    } else {
 //                  System.out.println("Se instertaron los datos a la DB ");
-                salidor = false;
+salidor = false;
+                    }
                 }
-          }
-            //------------------------------------------------------------------  
-              //Nos preparamos para realizar la copia del archivo ya almacenado
-              File rutaOriginalFichero;
-              File rutaDestinoFichero;
-              rutaOriginalFichero= new File("D:\\Franco\\JAVA\\Proyectos\\J_ContactAdministrator\\Contactos.csv");
-              rutaDestinoFichero = new File("D:\\Franco\\JAVA\\Proyectos\\J_ContactAdministrator\\ContactosLeidos\\Contactos.csv");
-              
-              InputStream in = new FileInputStream(rutaOriginalFichero);
-              OutputStream out = new FileOutputStream(rutaDestinoFichero);
-              //Usamos un buffer para la copia
-              byte[] buf = new byte[1024];
-              int len;
-              while ((len = in.read(buf)) > 0) {
+                //------------------------------------------------------------------
+                //Nos preparamos para realizar la copia del archivo ya almacenado
+                File rutaDestinoFichero;
+                rutaOriginalFichero= new File("D:\\Franco\\JAVA\\Proyectos\\J_ContactAdministrator\\Contactos.csv");
+                rutaDestinoFichero = new File("D:\\Franco\\JAVA\\Proyectos\\J_ContactAdministrator\\ContactosLeidos\\Contactos.csv");
+                InputStream in = new FileInputStream(rutaOriginalFichero);
+                OutputStream out = new FileOutputStream(rutaDestinoFichero);
+                //Usamos un buffer para la copia
+                byte[] buf = new byte[1024];
+                int len;
+                while ((len = in.read(buf)) > 0) {
                     out.write(buf, 0, len);
-              }
-              in.close();
-              out.close();
-              
-              fr.close();//cerramos el FileReader para poder borrar el archivo
+                } in.close();
+                out.close();
+                //cerramos el FileReader para poder borrar el archivo
+            }
             //------------------------------------------------------------------    
               //Aqui generamos el delete para no volver a importar el mismo archivo
               boolean borrarOriginal = rutaOriginalFichero.delete();
