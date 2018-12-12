d9efe6415272958e3e57db3759890f405411f8c0
galloafranco <galloafranco@gmail.com>
2018-12-12 20:59:29 -0300

    Se agrega clase test para coverage a sql server agenda dao

---
+++ test/j_contactadministrator/SQLServer_AgendaDAOTest.java
@@ -0,0 +1,82 @@
+package j_contactadministrator;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author FrancoDesk
+ */
+public class SQLServer_AgendaDAOTest {
+    
+    public SQLServer_AgendaDAOTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+
+    /**
+     * Test of addContacto method, of class SQLServer_AgendaDAO.
+     */
+    @Test
+    public void testAddContacto() {
+    }
+
+    /**
+     * Test of modificarContacto method, of class SQLServer_AgendaDAO.
+     */
+    @Test
+    public void testModificarContacto() throws Exception {
+    }
+
+    /**
+     * Test of eliminarContacto method, of class SQLServer_AgendaDAO.
+     */
+    @Test
+    public void testEliminarContacto() {
+    }
+
+    /**
+     * Test of eliminarLista method, of class SQLServer_AgendaDAO.
+     */
+    @Test
+    public void testEliminarLista() {
+    }
+
+    /**
+     * Test of mostrar method, of class SQLServer_AgendaDAO.
+     */
+    @Test
+    public void testMostrar() {
+    }
+
+    /**
+     * Test of exportarLista method, of class SQLServer_AgendaDAO.
+     */
+    @Test
+    public void testExportarLista() {
+    }
+
+    /**
+     * Test of ordenarAgenda method, of class SQLServer_AgendaDAO.
+     */
+    @Test
+    public void testOrdenarAgenda() {
+    }
+
+    /**
+     * Test of getByName method, of class SQLServer_AgendaDAO.
+     */
+    @Test
+    public void testGetByName() {
+    }
+    
+}
