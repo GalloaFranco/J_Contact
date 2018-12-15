02d43c07df65b60eed47c8800cdb2bf5e7481f72
galloafranco <galloafranco@gmail.com>
2018-12-15 18:23:44 -0300

    Se da soporte a test addContacto de SQLserverDAO, para ello se modifica tipo de valor de retorno de interface DAO y por ende sus @Override

--- test/j_contactadministrator/SQLServer_AgendaDAOTest.java
+++ test/j_contactadministrator/SQLServer_AgendaDAOTest.java
@@ -6,6 +6,11 @@ package j_contactadministrator;
  * and open the template in the editor.
  */
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import junit.framework.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -16,18 +21,34 @@ import static org.junit.Assert.*;
  */
 public class SQLServer_AgendaDAOTest {
     
+    SQLServer_AgendaDAO testSchedule;
+    
     public SQLServer_AgendaDAOTest() {
     }
     
     @Before
     public void setUp() {
+        testSchedule = new SQLServer_AgendaDAO();
     }
 
-    /**
-     * Test of addContacto method, of class SQLServer_AgendaDAO.
-     */
+
     @Test
     public void testAddContacto() {
+        try {
+            Contacto testContact = new Contacto("Testito", "Eltesteado", "Test", "Testito@test.com", "ambienteTestin10", "351616456");
+            testSchedule.addContacto(testContact);
+             
+            Pattern pattern = Pattern.compile("(TESTITO)");
+            Matcher matcher =  pattern.matcher(testSchedule.getByName("TESTITO"));
+           
+            Assert.assertEquals(false , matcher.matches());
+            
+            testSchedule.eliminarContacto(testContact.getNombre());
+            
+        } catch (InvalidEmailException | InvalidTelephoneNumberException | InvalidNameException ex) {
+            Logger.getLogger(SQLServer_AgendaDAOTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
     }
 
     /**
